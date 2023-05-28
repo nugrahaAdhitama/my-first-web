a7a4c5f2663ca8779ba96f1a6a70c5a362fb35ca
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-05-29 05:36:02 +0700

    verbously comment this code

--- server.js
+++ server.js
@@ -1,72 +1,77 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const { registerUser } = require("./utils/users");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const flash = require("connect-flash");
-const { v4: uuidv4 } = require("uuid");
-const { body, check, validationResult } = require("express-validator");
-const app = express();
-const port = 3000;
+// Mengimpor modul-modul yang diperlukan
+const express = require("express"); // Modul express digunakan untuk membuat aplikasi web
+const expressLayouts = require("express-ejs-layouts"); // Modul express-ejs-layouts digunakan untuk pengaturan tata letak dengan EJS
+const { registerUser } = require("./utils/users"); // Mengimpor fungsi registerUser dari file ./utils/users.js
+const session = require("express-session"); // Modul express-session digunakan untuk manajemen sesi
+const cookieParser = require("cookie-parser"); // Modul cookie-parser digunakan untuk parsing cookie
+const flash = require("connect-flash"); // Modul connect-flash digunakan untuk mengirim pesan flash antar permintaan
+const { v4: uuidv4 } = require("uuid"); // Mengimpor fungsi v4 dari modul uuid sebagai uuidv4
+const { body, check, validationResult } = require("express-validator"); // Mengimpor fungsi body, check, validationResult dari modul express-validator untuk validasi input
+const app = express(); // Membuat aplikasi Express baru
+const port = 3000; // Menentukan nomor port yang akan digunakan
 
-app.set("view engine", "ejs");
-app.use(expressLayouts);
+app.set("view engine", "ejs"); // Mengatur mesin rendering yang digunakan sebagai EJS
+app.use(expressLayouts); // Menggunakan modul express-ejs-layouts untuk pengaturan tata letak
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Menggunakan middleware express.json untuk parsing permintaan dengan tipe konten application/json
+app.use(express.urlencoded({ extended: true })); // Menggunakan middleware express.urlencoded untuk parsing permintaan dengan tipe konten application/x-www-form-urlencoded
 
-app.use(cookieParser("secret"));
+app.use(cookieParser("secret")); // Menggunakan modul cookie-parser dengan kunci "secret" untuk parsing cookie
 app.use(
   session({
-    cookie: { maxAge: 6000 },
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
+    cookie: { maxAge: 6000 }, // Menentukan masa berlaku cookie dalam milidetik
+    secret: "secret", // Menentukan kunci rahasia untuk mengenkripsi data sesi
+    resave: true, // Menentukan apakah sesi akan disimpan ulang pada setiap permintaan
+    saveUninitialized: true, // Menentukan apakah sesi akan disimpan bahkan jika tidak ada data yang ditetapkan
   })
 );
-app.use(flash());
+app.use(flash()); // Menggunakan modul connect-flash untuk mengirim pesan flash antar permintaan
 
 app.get("/", (req, res) => {
+  // Menangani permintaan GET ke route "/"
   res.render("index", {
-    title: "Home",
-    layout: "layouts/main-layout",
-    msg: req.flash("Success"),
+    title: "Home", // Mengatur judul halaman menjadi "Home"
+    layout: "layouts/main-layout", // Menggunakan tata letak "layouts/main-layout"
+    msg: req.flash("Success"), // Mengirimkan pesan flash dengan kunci "Success" ke tampilan
   });
 });
 
 app.post(
   "/register",
-  [body("username").notEmpty(), body("password").notEmpty()],
+  [body("username").notEmpty(), body("password").notEmpty()], // Menggunakan express-validator untuk memvalidasi input username dan password
   (req, res) => {
-    const errors = validationResult(req);
+    // Menangani permintaan POST ke route "/register"
+    const errors = validationResult(req); // Mengecek apakah terdapat kesalahan validasi pada input
     if (!errors.isEmpty()) {
+      // Jika terdapat kesalahan validasi
       res.render("index", {
-        title: "Home",
-        layout: "layouts/main-layout",
-        msg: req.flash("msg"),
-        errors: errors.array(),
+        title: "Home", // Mengatur judul halaman menjadi "Home"
+        layout: "layouts/main-layout", // Menggunakan tata letak "layouts/main-layout"
+        msg: req.flash("msg"), // Mengirimkan pesan flash dengan kunci "msg" ke tampilan
+        errors: errors.array(), // Mengirimkan array kesalahan validasi ke tampilan
       });
     } else {
-      const id = uuidv4();
-      req.body.id = id;
+      // Jika tidak terdapat kesalahan validasi
+      const id = uuidv4(); // Menghasilkan UUID v4 sebagai ID unik untuk user
+      req.body.id = id; // Menetapkan ID pada permintaan (req.body)
 
-      console.log(req.body);
+      console.log(req.body); // Menampilkan data permintaan di konsol
 
       const user = {
         id: req.body.id,
         username: req.body.username,
         password: req.body.password,
-      };
+      }; // Membuat objek user dari data permintaan
 
-      registerUser(user);
+      registerUser(user); // Memanggil fungsi registerUser untuk mendaftarkan user
 
-      req.flash("Success", "Registration Success");
+      req.flash("Success", "Registration Success"); // Mengirimkan pesan flash dengan kunci "Success" dan isi pesan "Registration Success"
 
-      res.redirect("/");
+      res.redirect("/"); // Mengalihkan pengguna kembali ke halaman utama ("/")
     }
   }
 );
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`); // Menampilkan pesan di konsol saat aplikasi berjalan
 });
