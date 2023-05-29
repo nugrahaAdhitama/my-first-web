6a0b6e7801c2a816cc26e4f51aa0526c01ab88eb
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-05-29 16:40:32 +0700

    remove the comment

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 // Mengimpor modul-modul yang diperlukan
-const express = require("express"); // Modul express digunakan untuk membuat aplikasi web
-const expressLayouts = require("express-ejs-layouts"); // Modul express-ejs-layouts digunakan untuk pengaturan tata letak dengan EJS
-const { registerUser, loginUser } = require("./utils/users"); // Mengimpor fungsi registerUser dari file ./utils/users.js
-const session = require("express-session"); // Modul express-session digunakan untuk manajemen sesi
-const cookieParser = require("cookie-parser"); // Modul cookie-parser digunakan untuk parsing cookie
-const flash = require("connect-flash"); // Modul connect-flash digunakan untuk mengirim pesan flash antar permintaan
-const { v4: uuidv4 } = require("uuid"); // Mengimpor fungsi v4 dari modul uuid sebagai uuidv4
-const { body, check, validationResult } = require("express-validator"); // Mengimpor fungsi body, check, validationResult dari modul express-validator untuk validasi input
-const path = require("path"); // Mengimpor modul path
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const { registerUser, loginUser } = require("./utils/users");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const { v4: uuidv4 } = require("uuid");
+const { body, check, validationResult } = require("express-validator");
+const path = require("path");
 
-const app = express(); // Membuat aplikasi Express baru
-const port = 3000; // Menentukan nomor port yang akan digunakan
+const app = express();
+const port = 3000;
 
-app.set("view engine", "ejs"); // Mengatur mesin rendering yang digunakan sebagai EJS
-app.use(expressLayouts); // Menggunakan modul express-ejs-layouts untuk pengaturan tata letak
-app.use(express.json()); // Menggunakan middleware express.json untuk parsing permintaan dengan tipe konten application/json
-app.use(express.urlencoded({ extended: true })); // Menggunakan middleware express.urlencoded untuk parsing permintaan dengan tipe konten application/x-www-form-urlencoded
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public"))); // Mengatur folder "public" sebagai folder statis yang akan digunakan untuk file seperti CSS, JavaScript, dan gambar
-
-app.use(cookieParser("secret")); // Menggunakan modul cookie-parser dengan kunci "secret" untuk parsing cookie
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser("secret"));
 app.use(
   session({
     cookie: { maxAge: null },
@@ -29,119 +28,109 @@ app.use(
   })
 );
 
-app.use(flash()); // Menggunakan modul connect-flash untuk mengirim pesan flash antar permintaan
+app.use(flash());
 
-// Fungsi middleware untuk memeriksa apakah pengguna terotentikasi
 const checkAuthenticated = (req, res, next) => {
   if (req.session.isAuthenticated) {
-    next(); // Lanjutkan ke langkah berikutnya jika pengguna terotentikasi
+    next();
   } else {
-    res.redirect("/login"); // Alihkan ke halaman login jika pengguna tidak terotentikasi
+    res.redirect("/login");
   }
 };
 
 app.get("/", (req, res) => {
-  // Menangani permintaan GET ke route "/"
   res.render("index", {
-    title: "Home", // Mengatur judul halaman menjadi "Home"
-    layout: "layouts/main-layout", // Menggunakan tata letak "layouts/main-layout"
-    js: "", // Tidak ada file JavaScript yang akan digunakan
+    title: "Home",
+    layout: "layouts/main-layout",
+    js: "",
   });
 });
 
 app.post(
   "/register",
-  [body("username").notEmpty(), body("password").notEmpty()], // Menggunakan express-validator untuk memvalidasi input username dan password
+  [body("username").notEmpty(), body("password").notEmpty()],
   (req, res) => {
-    // Menangani permintaan POST ke route "/register"
-    const errors = validationResult(req); // Mengecek apakah terdapat kesalahan validasi pada input
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      // Jika terdapat kesalahan validasi
       res.render("index", {
-        title: "Home", // Mengatur judul halaman menjadi "Home"
-        layout: "layouts/main-layout", // Menggunakan tata letak "layouts/main-layout"
-        msg: req.flash("msg"), // Mengirimkan pesan flash dengan kunci "msg" ke tampilan
-        errors: errors.array(), // Mengirimkan array kesalahan validasi ke tampilan
+        title: "Home",
+        layout: "layouts/main-layout",
+        msg: req.flash("msg"),
+        errors: errors.array(),
       });
     } else {
-      // Jika tidak terdapat kesalahan validasi
-      const id = uuidv4(); // Menghasilkan UUID v4 sebagai ID unik untuk user
-      req.body.id = id; // Menetapkan ID pada permintaan (req.body)
+      const id = uuidv4();
+      req.body.id = id;
 
-      console.log(req.body); // Menampilkan data permintaan di konsol
+      console.log(req.body);
 
       const user = {
         id: req.body.id,
         username: req.body.username,
         password: req.body.password,
-      }; // Membuat objek user dari data permintaan
+      };
 
-      registerUser(user); // Memanggil fungsi registerUser untuk mendaftarkan user
+      registerUser(user);
 
-      req.flash("Success", "Congratulation Registration Success"); // Mengirimkan pesan flash dengan kunci "Success" dan isi pesan "Congratulation Registration Success"
+      req.flash("Success", "Congratulation Registration Success");
 
-      res.redirect("/register/success"); // Mengalihkan pengguna ke halaman "/register/success"
+      res.redirect("/register/success");
     }
   }
 );
 
 app.get("/register/success", (req, res) => {
-  // Menangani permintaan GET ke route "/register/success"
   res.render("register-success", {
-    title: "Register Success", // Mengatur judul halaman menjadi "Register Success"
-    layout: "layouts/main-layout", // Menggunakan tata letak "layouts/main-layout"
-    js: "/js/register-succes.js", // Menggunakan file JavaScript "/js/register-succes.js"
-    illustration: "/assets/img/confirm.png", // Mengatur path gambar ilustrasi yang benar
-    msg: req.flash("Success"), // Mengirimkan pesan flash dengan kunci "Success" ke tampilan
+    title: "Register Success",
+    layout: "layouts/main-layout",
+    js: "/js/register-succes.js",
+    illustration: "/assets/img/confirm.png",
+    msg: req.flash("Success"),
   });
 });
 
 app.get("/register/success-redirect", (req, res) => {
-  // Menangani permintaan GET ke route "/register/success-redirect"
-  res.redirect("/login"); // Mengalihkan pengguna ke halaman "/login"
+  res.redirect("/login");
 });
 
 app.get("/login", (req, res) => {
-  // Menangani permintaan GET ke route "/login"
   res.render("login", {
-    title: "Login", // Mengatur judul halaman menjadi "Login"
-    layout: "layouts/main-layout", // Menggunakan tata letak "layouts/main-layout"
-    js: "", // Tidak ada file JavaScript yang akan digunakan
+    title: "Login",
+    layout: "layouts/main-layout",
+    js: "",
   });
 });
 
 app.post("/login", (req, res) => {
-  // Menangani permintaan POST ke route "/login"
   const user = {
     username: req.body.username,
     password: req.body.password,
   };
 
-  const result = loginUser(user.username, user.password); // Memanggil fungsi loginUser untuk memeriksa keberhasilan login
+  const result = loginUser(user.username, user.password);
   if (result.success) {
-    req.session.isAuthenticated = true; // Menandai pengguna sebagai terotentikasi di sesi
-    req.session.username = result.user.username; // Menyimpan username pengguna di sesi
+    req.session.isAuthenticated = true;
+    req.session.username = result.user.username;
 
-    req.flash("login-success", "Login Success!"); // Mengirimkan pesan flash dengan kunci "login-success" dan isi pesan "Login Success!"
-    res.redirect("/dashboard"); // Mengalihkan pengguna ke halaman "/dashboard"
+    req.flash("login-success", "Login Success!");
+    res.redirect("/dashboard");
   } else {
-    req.flash("login-failed", "Login Failed!"); // Mengirimkan pesan flash dengan kunci "login-failed" dan isi pesan "Login Failed!"
-    res.redirect("/login"); // Mengalihkan pengguna ke halaman "/login"
+    req.flash("login-failed", "Login Failed!");
+    res.redirect("/login");
   }
 });
 
 app.get("/dashboard", checkAuthenticated, (req, res) => {
-  // Menangani permintaan GET ke route "/dashboard"
-  console.log(req.session); // Menampilkan data sesi pengguna di konsol
+  console.log(req.session);
 
   res.render("dashboard", {
     title: "Dashboard",
     layout: "layouts/main-layout",
     js: "",
-    username: req.session.username, // Mengirimkan username pengguna ke tampilan
+    username: req.session.username,
   });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`); // Menampilkan pesan di konsol saat aplikasi berjalan
+  console.log(`Example app listening on port ${port}`);
 });
--- utils/users.js
+++ utils/users.js
@@ -1,70 +1,56 @@
-// Mengimpor modul-modul yang diperlukan
-const fs = require("fs"); // Modul fs digunakan untuk berinteraksi dengan sistem berkas (file system)
-const path = require("path"); // Modul path digunakan untuk memanipulasi jalur (path)
-const bcrypt = require("bcrypt"); // Modul bcrypt digunakan untuk hashing password
+const fs = require("fs");
+const path = require("path");
+const bcrypt = require("bcrypt");
 
-// Mendefinisikan fungsi registerUser sebagai fungsi async dengan parameter user
 const registerUser = async (user) => {
-  // Mendapatkan jalur direktori data dengan menggabungkan jalur saat ini (__dirname) dengan "../data"
   const dataDir = path.join(__dirname, "../data");
-  // Mendapatkan jalur file users.json dengan menggabungkan jalur direktori data dengan "users.json"
   const file = path.join(dataDir, "users.json");
 
-  // Mengecek apakah direktori data sudah ada, jika tidak, maka buat direktori tersebut
   if (!fs.existsSync(dataDir)) {
     fs.mkdirSync(dataDir);
   }
 
   let users;
-  // Mengecek apakah file users.json sudah ada, jika ada, maka baca isi file tersebut
+
   if (fs.existsSync(file)) {
     users = JSON.parse(fs.readFileSync(file));
   } else {
-    // Jika file users.json tidak ada, inisialisasi users sebagai array kosong
     users = [];
   }
 
-  // Menghasilkan salt untuk digunakan dalam proses hashing password
   const salt = await bcrypt.genSalt(10);
-  // Melakukan hashing terhadap password yang diberikan menggunakan salt yang dihasilkan
   const hashedPassword = await bcrypt.hash(user.password, salt);
 
-  // Menambahkan data user baru ke dalam array users
   users.push({
     id: user.id,
     username: user.username,
     password: hashedPassword,
   });
 
-  // Menulis kembali file users.json dengan data users yang sudah ditambahkan
   fs.writeFileSync(file, JSON.stringify(users, null, 2));
 };
 
-// Mendefinisikan fungsi loginUser dengan parameter username dan password
 const loginUser = (username, password) => {
-  const dataDir = path.join(__dirname, "../data"); // Mendapatkan jalur direktori data dengan menggabungkan jalur saat ini (__dirname) dengan "../data"
-  const file = path.join(dataDir, "users.json"); // Mendapatkan jalur file users.json dengan menggabungkan jalur direktori data dengan "users.json"
+  const dataDir = path.join(__dirname, "../data");
+  const file = path.join(dataDir, "users.json");
 
   if (fs.existsSync(file)) {
-    const users = JSON.parse(fs.readFileSync(file)); // Membaca isi file users.json dan mengurai JSON menjadi objek users
+    const users = JSON.parse(fs.readFileSync(file));
 
-    const user = users.find((user) => user.username === username); // Mencari user dengan username yang sesuai dalam objek users
+    const user = users.find((user) => user.username === username);
 
     if (user) {
-      // Jika user ditemukan
       if (bcrypt.compareSync(password, user.password)) {
-        // Membandingkan password yang dimasukkan dengan password yang di-hash di file
-        return { success: true, user, message: "Login successful!" }; // Mengembalikan objek dengan status login berhasil, user yang sesuai, dan pesan sukses
+        return { success: true, user, message: "Login successful!" };
       } else {
-        return { success: false, message: "Invalid username or password!" }; // Mengembalikan objek dengan status login gagal dan pesan kesalahan
+        return { success: false, message: "Invalid username or password!" };
       }
     } else {
-      return { success: false, message: "Invalid username or password!" }; // Mengembalikan objek dengan status login gagal dan pesan kesalahan
+      return { success: false, message: "Invalid username or password!" };
     }
   } else {
-    return { success: false, message: "User database not found!" }; // Mengembalikan objek dengan status login gagal dan pesan kesalahan karena database pengguna tidak ditemukan
+    return { success: false, message: "User database not found!" };
   }
 };
 
-// Mengekspor fungsi registerUser, loginUser agar dapat digunakan pada modul lain
 module.exports = { registerUser, loginUser };
